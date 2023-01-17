38c14cdc7b03e8b06d056f595e6a295c1c9edbc0
Erik Somnäs <erik.somnas@gmail.com>
2023-01-17 17:30:33 +0100

    add EB_Garamond google font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { EB_Garamond } from '@next/font/google';
+
+const garamond = EB_Garamond({
+  subsets: ['latin']
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={garamond.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
